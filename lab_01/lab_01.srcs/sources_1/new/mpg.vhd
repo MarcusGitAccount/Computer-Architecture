27a0e8dbe013d88ac810852e1e08d4787f72f1d1
MarcusGitAccount <pop_marcus_98@yahoo.com>
2019-03-06 15:38:50 +0200

    seven segment display component

--- lab_01/lab_01.srcs/sources_1/new/mpg.vhd
+++ lab_01/lab_01.srcs/sources_1/new/mpg.vhd
@@ -14,7 +14,7 @@
 -- 
 -- Revision:
 -- Revision 0.01 - File Created
--- Additional Comments:
+-- Additional Comments: Monopulse generator implementation
 -- 
 ----------------------------------------------------------------------------------
 
@@ -23,15 +23,6 @@ library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
 use IEEE.STD_LOGIC_ARITH.ALL;
 use IEEE.STD_LOGIC_UNSIGNED.ALL;
-
--- Uncomment the following library declaration if using
--- arithmetic functions with Signed or Unsigned values
---use IEEE.NUMERIC_STD.ALL;
-
--- Uncomment the following library declaration if instantiating
--- any Xilinx leaf cells in this code.
---library UNISIM;
---use UNISIM.VComponents.all;
 
 entity mpg is
   port (
